be5cec71218506fbd6383ca75b9467fed02b2c95
mickael van der beek <kollektiv@kollektiv.local>
2013-09-06 14:36:46 +0200

    Removed unnecessary files

--- swiss_knife/seo/Tourist/SitemapXMLtemplates.js
+++
@@ -1,9 +0,0 @@
-exports.templateJSON = {
-	_name: 'sitemapindex',
-	_attrs: {
-		xmlns: 'http://www.sitemaps.org/schemas/sitemap/0.9',
-		'xmlns:xsi' : 'http://www.w3.org/2001/XMLSchema-instance',
-		'xsi:schemaLocation': 'http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/siteindex.xsd'
-	},
-	_content: []
-};
