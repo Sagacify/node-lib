a22741b451554357a07efe5b5a306c8c762df262
mickael van der beek <kollektiv@kollektiv.local>
2013-10-29 21:15:59 +0100

    special values

---
+++ fuzzing/special_values.js
@@ -0,0 +1,112 @@
+var special_Strings = [
+	'',
+	'˙',
+	'<',
+	'>',
+	';',
+	'&',
+	'@',
+	'\\',
+	'\n',
+	'\r',
+	'\n\r',
+	'%00',
+	'null',
+	'ﬂ∂∏ı',
+	'void',
+	'void "a"',
+	'void 1',
+	new String()
+];
+
+var special_Numbers = [
+	0,
+	-1,
+	1,
+	Infinity,
+	Number.NaN,
+	Number.NEGATIVE_INFINITY,
+	Number.POSITIVE_INFINITY,
+	Number.MAX_VALUE,
+	Number.MAX_VALUE + Number.MAX_VALUE,
+	Number.MIN_VALUE - Number.MIN_VALUE,
+	Number.MIN_VALUE + Number.MAX_VALUE,
+	Number.MAX_VALUE - Number.MIN_VALUE
+];
+
+var special_Booleans = [
+	true,
+	false,
+	new Boolean()
+];
+
+function primitive_undefined () {
+	return;
+}
+
+var special_Primitives = [
+	null,
+	undefined,
+	primitive_undefined(),
+	void 1
+];
+
+var special_Objects = [
+	{},
+	new Object(),
+	new Object(null),
+	new Object(undefined)
+];
+
+var special_Arrays = [
+	[],
+	new Array()
+];
+
+function get_filled_Objects () {
+	var filled_Objects = [];
+	var object;
+	for(var i = 0; i < special_Strings; i++) {
+		object = {};
+		object[special_Strings[i]] = true;
+		filled_Objects[i].push(object);
+	}
+	return filled_Objects;
+}
+
+function get_filled_Array () {
+	var filled_Array = [];
+	var array;
+	for(var i = 0; i < special_Numbers; i++) {
+		array = [];
+		array[special_Numbers[i]] = true;
+		filled_Array[i].push(object);
+	}
+	return filled_Array;
+}
+
+var results = {
+	'Array': special_Arrays.concat(get_filled_Array()),
+	'String': special_Strings,
+	'Number': special_Numbers,
+	'Object': special_Objects.concat(get_filled_Objects()),
+	'Boolean': special_Booleans,
+	'Primitives': special_Primitives
+};
+
+function gen_all_special_Values () {
+	var all_values = [];
+	var keys = Object.keys(results);
+	var len = keys.length;
+	var i;
+	while(len--) {
+		i = keys[len];
+		all_values.concat(results[i]);
+	}
+	return all_values;
+}
+
+results.all_special_values = gen_all_special_Values();
+results.too_long_string = new Buffer(0xFFFD).toString();
+
+module.exports = results;
