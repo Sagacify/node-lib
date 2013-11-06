11e0f3e9318158a5cf2c82257aad840b43e6c9f5
Yvan de Ghellinck <yvan@ghellinck.be>
2013-11-06 13:27:18 +0100

    jkl

--- model_share/model_controller.js
+++ model_share/model_controller.js
@@ -2,19 +2,19 @@ var apiRecorder = require('./api_recorder');
 
 module.exports = function (app) {
 
-	app.SGget('/api/app_models', {
-		auth: false
-	}, function(req, res){
-		var routes = apiRecorder.routes;
-		var schemas = {};
-		mongoose.models.keys().forEach(function(model){
-			if(model != 'Bug'){
-				schemas[model] = mongoose.models[model].schema.getClientFormat();
-				schemas[model].collection.name = mongoose.collectionNameFromModelName(model);
-			}
-		});
-		var structure = {routes: routes, schemas:schemas};
-		res.SGsend(structure);
-	});
+	// app.SGget('/api/app_models', {
+	// 	auth: false
+	// }, function(req, res){
+	// 	var routes = apiRecorder.routes;
+	// 	var schemas = {};
+	// 	mongoose.models.keys().forEach(function(model){
+	// 		if(model != 'Bug'){
+	// 			schemas[model] = mongoose.models[model].schema.getClientFormat();
+	// 			schemas[model].collection.name = mongoose.collectionNameFromModelName(model);
+	// 		}
+	// 	});
+	// 	var structure = {routes: routes, schemas:schemas};
+	// 	res.SGsend(structure);
+	// });
 
 };
