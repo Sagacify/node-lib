96370c404b978150cfdd6cf425b1b0fae9dbdc45
fbeuvens <francois.beuven>
2013-11-08 14:03:14 +0100

    log out

--- routes_recorder/api_recorder.js
+++ routes_recorder/api_recorder.js
@@ -6,11 +6,7 @@ exports.routes = routes;
 
 exports.addRoute = function(method, uri, options){
 	if(uri && uri.startsWith('/api') && uri != '/api/app_models'){
-		console.log(options.validation)
 		options.validation = SGStrictTyping.develop_ValidationConfig(options.validation);
-		console.log(method)
-		console.log(uri)
-		console.log(options.validation)
 		routes[method][uri] = options;
 	}
 };
