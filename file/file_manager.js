e7be9c34d07255aafaef4ed2c64134ad59cb189d
fbeuvens <francois.beuven>
2013-11-11 16:38:26 +0100

    file manager

--- file/file_manager.js
+++ file/file_manager.js
@@ -38,7 +38,7 @@ exports.s3BucketInitialization = function(){
 
 exports.writeFileToS3 = function (base64data, extension, secure, callback){
 	var name = uuid.v4();
-	var filename = extension?name+"."+extension:extension;
+	var filename = extension?name+"."+extension:name;
 	s3.client.putObject({
 		Bucket: secure?config.AWS.s3SecuredBucketName:config.AWS.s3BucketName,
 		Key: filename,
