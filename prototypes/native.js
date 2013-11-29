be74e1eafd2920057d80ca0b2d20f435b908badf
François Beuvens <francois.beuvens@gmail.com>
2013-11-29 09:15:20 +0100

    misc

---
+++ prototypes/native.js
@@ -0,0 +1,6 @@
+require('./native/array');
+require('./native/date');
+require('./native/string');
+require('./native/function');
+require('./native/object');
+require('./native/console');
---
+++ prototypes/native/console.js
@@ -0,0 +1,5 @@
+// console._log = console.log;
+// console.log = function(text){
+// 	console._log(text)
+// 	console._log(new Error().stack)
+// }
