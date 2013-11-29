77fb69c828ae4eaef022dabca8813d09a32bd417
Kevin Françoisse <kfrancoi@gmail.com>
2013-11-29 10:11:58 +0100

    sms

--- mail/SMSer.js
+++ mail/SMSer.js
@@ -1,15 +1,30 @@
-var client = require('twilio')(config.sms.accountSid, config.sms.authToken);
- 
-
+//var client = require('twilio')(config.sms.accountSid, config.sms.authToken);
+//var client = require('twilio')("ACa2a197984dbe6e2132fcf0ea2a140f24", "684d13d663c6e335a149f4be54f250c1"); //Test
+var client = require('twilio')("AC0f3edf2d17a09848c65003cd66bd9982", "8b2575130fe9b6fd8c948078ecb234ab");
 //Send an SMS text message
 //@callback(err, responseData)
 function sendSMS(to, body, callback){
 	client.sms.messages.create({
 		to:to, // The phone numver we want to deliver the message to
-		from: config.sms.from, // A number bought from Twilio that is used for outbound communication
+		from: "+32470337816", // A number bought from Twilio that is used for outbound communication
 		body: body // body of the SMS message
-		}, callback);
+		}, function(err, res){
+			console.log("ERR");
+			console.log(err);
+			console.log("RES");
+			console.log(res);
+		});
 }
+
+// function sendSMS(to, body, callback){
+// 	client.sms.messages.create({
+// 		to:to, // The phone numver we want to deliver the message to
+// 		from: config.sms.from || "+32496961218", // A number bought from Twilio that is used for outbound communication
+// 		body: body // body of the SMS message
+// 		}, callback);
+// }
+
+sendSMS("+32496961218", "Hello World");
 
 //ERROR CODE :
 //This phone number is invalid.	21211
