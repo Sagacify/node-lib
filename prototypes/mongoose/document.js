f0ad5acc7aeb4fd59bd83fa686e6ccdf1018436a
François Beuvens <francois.beuvens@gmail.com>
2013-11-30 01:59:22 +0100

    console doc

--- prototypes/mongoose/document.js
+++ prototypes/mongoose/document.js
@@ -12,8 +12,6 @@ mongoose.Document.prototype.toObject = function(opts){
 	return obj;
 };
 
-var async = require('async');
-
 mongoose.Document.prototype.created_at = function() {
 	if(this.schema.tree._id.type.name == 'ObjectId')
  		return new Date(parseInt(this._id.toString().slice(0,8), 16) * 1000);
@@ -23,88 +21,37 @@ mongoose.Document.prototype.created_at = function() {
  		return null;
 };
 
-mongoose.Document.prototype.will = function(meth, path, args, callback){
-	var willMeth = "will"+meth.capitalize();
-	if(path){
-		var methPath = meth.endsWith('Array')?meth.replace('Array', path.capitalize()):meth+path.capitalize();
-		var willMethPath = "will"+methPath.capitalize();
-	}
-	else{
-		var willMethPath = willMeth;
-	}
-	var willRes = typeof this[willMethPath] == "function"?this[willMethPath](args):this[willMeth](path, args);
-	if(willRes instanceof Error || willRes instanceof SGError){
-		if(callback)
-			callback(willRes);
-		return false;
-	}
-	else{
-		return true;
-	}
+mongoose.Document.prototype.didDo = function(action, params){
+	if(typeof this["did"+action.capitalize()] == "function")
+		return this["did"+action.capitalize()]._apply(this, params);
 };
 
-mongoose.Document.prototype.did = function(meth, path, args, callback){
-	var didMeth = "did"+meth.capitalize();
-	if(path){
-		var methPath = meth.endsWith('Array') ? meth.replace('Array', path.capitalize()) : meth + path.capitalize();
-		var didMethPath = "did"+methPath.capitalize();
-	}
-	else{
-		var didMethPath = didMethPath;
-	}
-	var me = this;
-	return function (err, result) {
-		if(!err) {
-			if(typeof me[didMethPath] == "function") {
-				me[didMethPath](args);
-			}
-			else {
-				me[didMeth](path, args);
-			}
-		}
-		if(callback) {
-			callback(null, result);
-		}
-		else {
-			return result;
-		}
-	};
+mongoose.Document.prototype.isSemiEmbedded = function(path){
+	var schema = this.schema.tree._get(path);
+	return schema&&schema._id&&schema._id.ref;
+};
+
+mongoose.Document.prototype.isSemiEmbeddedArray = function(path){
+	var schema = this.schema.tree._get(path+'.0');
+	return schema&&schema._id&&schema._id.ref;
+};
+
+mongoose.Document.prototype.isRef = function(path){
+	var schema = this.schema.tree._get(path);
+	return schema&&schema.ref;
+};
+
+mongoose.Document.prototype.isRefArray = function(path){
+	var schema = this.schema.tree._get(path)[0];
+	return schema&&schema.ref;
+};
+
+mongoose.Document.prototype.getRef = function(path){
+	var schema = this.schema.tree._get(path)[0];
+	return schema.ref;
 };
 
 mongoose.Document.prototype._get = mongoose.Document.prototype.get;
-
-// mongoose.Document.prototype.get = function(path, params, callback){
-// 	if(typeof params == "function"){
-// 		callback = params;
-// 		params = {};
-// 	}
-
-// 	if(!callback){
-// 		var willRes = this.willGet(path, params);
-// 		if(willRes instanceof Error || willRes instanceof SGError){
-// 			return willRes;
-// 		}
-// 		var val = this.doGet(path, params);
-// 		this.didGet(path, params);
-// 		return val;
-// 	}
-// 	else{
-// 		var me = this;
-// 		this.willGet(path, params, function(err){
-// 			if(!err){
-// 				me.doGet(path, params, function(err, res){
-// 					if(!err){
-// 						me.didGet(path, params);
-// 					}
-// 					callback(err, res);
-// 				})
-// 			}
-// 			else{
-// 				callback(err);
-// 			}
-// 		});
-// 	}
-// };
 
 mongoose.Document.prototype.willGet = function(path, params, callback){
 	if(typeof this["willGet"+path.capitalize()] == "function"){
@@ -134,79 +81,9 @@ mongoose.Document.prototype.doGet = function(path, params, callback){
     }
 };
 
-// mongoose.Document.prototype.get = function(path, options, callback){
-// 	if(typeof options == "function"){
-// 		callback = options;
-// 		options = {};
-// 	}
-// 	options = options||{};
-
-// 	var getPath = "get"+path.capitalize();
-
-// 	if(!this.will('get', path, options.params, callback)){
-// 		return;
-// 	}
-
-// 	var me = this;
-// 	var post = this.did('get', path, options.params, callback);
-
-// 	if(typeof this[getPath] == "function"){
-// 		if(callback)
-// 			this[getPath]._apply(this, options.params, post);
-// 		else
-// 			return post(null, this[getPath]._apply(this, options.params))
-// 	}
-// 	else{
-// 		return post(null, this._get(path, options));
-// 	}
-// };
-
 mongoose.Document.prototype.didGet = function(path, params){
 	if(typeof this["didGet"+path.capitalize()] == "function")
 		return this["didGet"+path.capitalize()]._apply(this, params);
-};
-
-mongoose.Document.prototype.willDo = function(action, params, callback){
-	if(typeof this["will"+action.capitalize()] == "function"){
-		return this["will"+action.capitalize()]._apply(this, params, callback);
-	}
-	else{
-		return callback?callback(null):null;
-	}
-};
-
-// mongoose.Document.prototype.do = function(action, params, callback){
-// 	var willDoPath = "will"+action.capitalize();
-
-// 	if(!this.will('do', action, params, callback)){
-// 		return;
-// 	}
-
-// 	var post = this.did('do', action, params, callback);
-
-// 	if(typeof this[action] == "function" && this.schema.documentActions[action]){
-// 		if(callback)
-// 			this[action]._apply(this, params, post);
-// 		else
-// 			return post(null, this[action]._apply(this, params));
-// 	}
-// 	else{
-// 		callback(new SGError());
-// 	}
-// };
-
-mongoose.Document.prototype.doDo = function(action, params, callback){
-	if(typeof this[action] == "function" && this.schema.documentActions[action]){
-		this[action]._apply(this, params, callback);
-    }
-    else{
-		callback(new SGError());
-    }
-};
-
-mongoose.Document.prototype.didDo = function(action, params){
-	if(typeof this["did"+action.capitalize()] == "function")
-		return this["did"+action.capitalize()]._apply(this, params);
 };
 
 mongoose.Document.prototype.willSet = function(path, val, callback){
@@ -221,50 +98,6 @@ mongoose.Document.prototype.willSet = function(path, val, callback){
 };
 
 mongoose.Document.prototype._set = mongoose.Document.prototype.set;
-
-// mongoose.Document.prototype.set = function set(path, val, type, options, callback){
-// 	if(path && path.isObject()){
-// 		var me = this;
-// 		if(!callback && typeof val == "function")
-// 			callback = val;
-// 		async.each(path.keys(), function(key, callback){
-// 			me.set(key, path[key], null, null, callback);
-// 		}, function(err){
-// 			if(callback)
-// 				callback(err);
-// 		});
-// 	}
-// 	else{
-// 		var setPath = "set"+path.capitalize();
-
-// 		if(typeof this[setPath] == "function" && set.caller != this[setPath]){
-// 			if(this[setPath].hasCallback() && callback){
-// 				this[setPath](val, callback);	
-// 			}
-// 			else{
-// 				this[setPath](val);
-// 				if(callback)
-// 					callback();
-// 			}
-// 		}
-// 		else {
-// 			if(!this.will('set', path, val, callback)){
-// 				return;
-// 			}
-
-// 			var post = this.did('set', path, val, callback);
-
-// 			if(this.schema.tree._get(path) && this.schema.tree._get(path)._id){
-// 				this.setSemiEmbedded(path, val, post);
-// 			}
-// 			else{
-// 				this._set.apply(this, arguments);
-// 				post();
-// 			}
-// 		}
-// 	}
-// },
-
 
 mongoose.Document.prototype.doSet = function(path, val, type, options, callback){
 	if(arguments.callee.caller.caller == this._set){
@@ -299,6 +132,12 @@ mongoose.Document.prototype.doSet = function(path, val, type, options, callback)
 		else if(this.schema.tree._get(path) && this.schema.tree._get(path)._id){
 			this.setSemiEmbedded(path, val, callback);
 		}
+		else if(this.schema.tree._get(path) && this.schema.tree._get(path).ref == 'File' && this.schema.tree._get(path).kind == 'Image'){
+			this.setBase64File(path, val, callback);
+		}
+		else if(this.schema.tree._get(path) instanceof Array && this.schema.tree._get(path)[0].ref == 'File' && this.schema.tree._get(path)[0].kind == 'Image'){
+			this.setBase64Files(path, val, callback);
+		}
 		else{
 			//this._set.apply(this, arguments);
 			this._set(path, val)
@@ -313,31 +152,6 @@ mongoose.Document.prototype.didSet = function(path, val){
 		return this["didSet"+path.capitalize()]._apply(this, {val:val});
 };
 
-mongoose.Document.prototype.isSemiEmbedded = function(path){
-	var schema = this.schema.tree._get(path);
-	return schema&&schema._id&&schema._id.ref;
-};
-
-mongoose.Document.prototype.isSemiEmbeddedArray = function(path){
-	var schema = this.schema.tree._get(path+'.0');
-	return schema&&schema._id&&schema._id.ref;
-};
-
-mongoose.Document.prototype.isRef = function(path){
-	var schema = this.schema.tree._get(path);
-	return schema&&schema.ref;
-};
-
-mongoose.Document.prototype.isRefArray = function(path){
-	var schema = this.schema.tree._get(path)[0];
-	return schema&&schema.ref;
-};
-
-mongoose.Document.prototype.getRef = function(path){
-	var schema = this.schema.tree._get(path)[0];
-	return schema.ref;
-};
-
 mongoose.Document.prototype.setSemiEmbedded = function(path, val, callback){
 	var me = this;
 	var set = function(doc){
@@ -349,7 +163,7 @@ mongoose.Document.prototype.setSemiEmbedded = function(path, val, callback){
 	if(val instanceof Object){
 		set(val);
 		if(callback)
-			callback(null);
+			callback();
 	}
 	else{
 		model(this.schema.tree._get(path)._id.ref).findById(val, function(err, doc){
@@ -360,6 +174,74 @@ mongoose.Document.prototype.setSemiEmbedded = function(path, val, callback){
 				callback(err);
 		});
 	}
+};
+
+var base64File = function(args, callback){
+	if(typeof args == "string"){
+		args = {
+			base64data: args,
+			extension: 'png'
+		}
+	}
+	var me = this;
+	var FileModel = model('File');
+	FileModel.createFile(args, callback);
+};
+
+mongoose.Document.prototype.setBase64File = function(path, val, callback){
+	if(!val || typeof val == "string" && val.startsWith('http')){
+		this._set(path, val);
+		if(callback)
+			callback();
+	}
+	else{
+		var me = this;
+		base64File(val, function(err, file){
+			me._set(path, file._id);
+			if(callback)
+				callback();
+		});
+	}
+};
+
+mongoose.Document.prototype.setBase64Files = function(path, vals, callback){
+	if(vals instanceof Array && !vals.length || vals instanceof Array && typeof vals[0] == "string" && vals[0].startsWith('http')){
+		this._set(path, vals);
+		if(callback)
+			callback();
+	}
+	else{
+		var me = this;
+		var files = [];
+		async.each(vals.keys(), function(key, callback){
+			base64File(vals[key], function(err, file){
+				files[key] = file;
+				callback(err);
+			});
+		}, function(err){
+			me._set(path, files);
+			if(callback)
+				callback();
+		});
+	}
+};
+
+mongoose.Document.prototype.willDo = function(action, params, callback){
+	if(typeof this["will"+action.capitalize()] == "function"){
+		return this["will"+action.capitalize()]._apply(this, params, callback);
+	}
+	else{
+		return callback?callback(null):null;
+	}
+};
+
+mongoose.Document.prototype.doDo = function(action, params, callback){
+	if(typeof this[action] == "function" && this.schema.documentActions[action]){
+		this[action]._apply(this, params, callback);
+    }
+    else{
+		callback(new SGError());
+    }
 };
 
 mongoose.Document.prototype.willAddInArray = function(path, val, callback){
@@ -410,35 +292,6 @@ mongoose.Document.prototype.doAddInArray = function(path, val, callback){
 			callback(null, this.get(path).last());
 	}
 };
-
-// mongoose.Document.prototype.addInArray = function(path, val, callback){
-// 	if(!this.will('addInArray', path, val, callback)){
-// 		return;
-// 	}
-
-// 	var post = this.did('addInArray', path, val, callback);
-
-// 	var addPath = "addIn"+path.capitalize();
-// 	if(typeof this[addPath] == "function"){
-// 		if(this[addPath].hasCallback()){
-// 			this[addPath](val, post);
-// 		}
-// 		else{
-// 			this[addPath](val);
-// 			post(null);
-// 		}
-// 	}
-// 	else if(this.isSemiEmbeddedArray(path)){
-// 		this.get(path).addSemiEmbedded(val, post);
-// 	}
-// 	else if(this.isRefArray(path)){
-// 		this.addInRefArray(path, val, post);
-// 	}
-// 	else{
-// 		this.get(path).push(val);
-// 		post(null, this.get(path).last());
-// 	}
-// };
 
 mongoose.Document.prototype.didAddInArray = function(path, val){
 	if(typeof path == "string" && typeof this["didAddIn"+path.capitalize()] == "function")
@@ -494,35 +347,6 @@ mongoose.Document.prototype.doRemoveFromArray = function(path, val, callback){
 	}
 };
 
-// mongoose.Document.prototype.removeFromArray = function(path, val, callback){
-// 	if(!this.will('removeFromArray', path, val, callback)){
-// 		return;
-// 	}
-
-// 	var post = this.did('removeFromArray', path, val, callback);
-
-// 	var removePath = "removeFrom"+path.capitalize();
-
-// 	var me = this;
-// 	if(typeof this[removePath] == "function"){
-// 		if(this[removePath].hasCallback()){
-// 			this[removePath](val, post);
-// 		}
-// 		else{
-// 			this[removePath](val);
-// 			post(null);
-// 		}
-// 	}
-// 	else if(val instanceof mongoose.Document){
-// 		val.remove();
-// 		post(null);
-// 	}
-// 	else{
-// 		this.get(path).sgRemove(val);
-// 		post(null);
-// 	}
-// };
-
 mongoose.Document.prototype.didRemoveFromArray = function(path, val){
 	if(typeof path == "string" && typeof this["didRemoveFrom"+path.capitalize()] == "function")
 		return this["didRemoveFrom"+path.capitalize()](this, val);
@@ -552,27 +376,6 @@ mongoose.Document.prototype.addInRefArray = function(path, val, callback){
 mongoose.Document.prototype.willUpdate = function(args, callback){
 	return callback?callback(null):null;
 };
-
-// mongoose.Document.prototype.sgUpdate = function(args, callback){
-// 	if(!this.will('update', null, args, callback)){
-// 		return;
-// 	}
-
-// 	var post = this.did('update', null, args, callback);
-
-// 	var me = this;
-// 	this.set(args, function(err){
-// 		if(!err){
-// 			//me.ensureUpdateConsistency();
-// 			me.save(function(err){
-// 				post(err, me);
-// 			});
-// 		}
-// 		else{
-// 			post(err);
-// 		}
-// 	});
-// };
 
 mongoose.Document.prototype.doUpdate = function(args, callback){
 	var me = this;
--- prototypes/native/console.js
+++ prototypes/native/console.js
@@ -1,5 +1,15 @@
 // console._log = console.log;
 // console.log = function(text){
-// 	console._log(text)
-// 	console._log(new Error().stack)
+// 	var stack = new Error().stack;
+// 	var fullLine = stack.split("\n")[2];
+// 	var line;
+// 	if(fullLine.indexOf('(') != -1){
+// 		var str = stack.split("\n")[2].split('(')[1];
+// 		line = str.substring(0, str.length-1);
+// 	}
+// 	else{
+// 		line = fullLine.trim(3);
+// 	}
+// 	console._log(line);
+// 	console._log(text+'\n');
 // }
