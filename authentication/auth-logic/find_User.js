befa1f595954b622bf278fb006aa1d7b505c5aed
mickael van der beek <kollektiv@kollektiv.local>
2013-12-04 18:54:54 +0100

    erge

--- authentication/auth-logic/find_User.js
+++ authentication/auth-logic/find_User.js
@@ -2,18 +2,24 @@ var UserModel = model('User');
 
 module.exports = function (mixin, callback) {
 	var search = {};
-	search[mixin.search[0]] = mixin.search[1];
-	UserModel.find(search, function (e, users) {
-		if(e) {
-			callback('COULDNT_FIND_USER');
-		}
-		else if((mixin.action !== 'Register') && (!users || !users.length)) {
-			callback('INVALID_ATTR_COMBINATION');
-		}
-		else {
-			mixin.users = users;
-			mixin.user = users[0];
-			callback(null, mixin);
-		}
-	});
+	var qs = mixin.search[1];
+	if((typeof qs === 'string') && qs.length) {
+		search[mixin.search[0]] = qs.toLowerCase();
+		UserModel.find(search, function (e, users) {
+			if(e) {
+				callback('COULDNT_FIND_USER');
+			}
+			else if((mixin.action !== 'Register') && (!users || !users.length)) {
+				callback('INVALID_ATTR_COMBINATION');
+			}
+			else {
+				mixin.users = users;
+				mixin.user = users[0];
+				callback(null, mixin);
+			}
+		});
+	}
+	else {
+		callback('INVALID_QUERY_STRING');
+	}
 };
