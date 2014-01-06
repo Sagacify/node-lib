29c4689120f88d8869932e5febaafbcb9318825a
Simon Debaecke <simon@sagacify.com>
2014-01-06 16:47:29 +0100

    child_process node

---
+++ child_process/child_process.js
@@ -0,0 +1,24 @@
+var is = require('../strict_typing/validateType');
+var exec = require('child_process').exec;
+
+exports.execute = function (command, callback) {
+	var child = exec(command, function (error, stdout, stderr) {
+		if (error) {
+			console.log('exec error: ' + error);
+		}
+		if (is.Function(callback)) {
+			callback.call(this, stderr, stdout);
+		} else {
+			if (stdout && stdout.length) {
+				console.log('stdout: ' + stdout);
+			}
+			if (stderr && stderr.length) {
+				console.log('stderr: ' + stderr);
+			}
+		}
+	});
+
+	child.stdout.on('data', function (data) {
+		// do something with data
+	});
+};
