9b55ce06dbd0e26a23caa34ec409a4b76986ec7f
Mickael van der Beek <mickael@sagacify.com>
2014-01-09 19:00:49 +0100

    removed comments

--- authentication/auth-logic/create_User.js
+++ authentication/auth-logic/create_User.js
@@ -9,14 +9,11 @@ module.exports = function (mixin, callback) {
 		state: user.state
 	});
 	delete user.state;
-	console.log('PASS - 0');
 	mixin.user[mixin.user.firstSet ? 'firstSet' : 'set'](user, function (e) {
 		if(e) {
-			console.log('PASS - 5');
 			callback(e);
 		}
 		else {
-			console.log('PASS - 6');
 			callback(null, mixin);
 		}
 	});
