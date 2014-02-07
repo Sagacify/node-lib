130c7768b32ee88e67226347136b76b3ef3139c0
Mickael van der Beek <mickael@sagacify.com>
2014-02-07 17:00:45 +0100

    comment support

--- communication/examples/send_with_SES.js
+++ communication/examples/send_with_SES.js
@@ -2,47 +2,39 @@ var Flash = require('../Flash');
 
 var Flash = new Flash({
 	email: {
-		// sender: {
-		// 	ses: {
-		// 		AWSAccessKeyID: 'AKIAIMQVLS3FLM3WQDXA',
-		// 		AWSSecretKey: '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'
-		// 	}
-		// },
-		receiver: {
-
+		sender: {
+			ses: {
+				AWSAccessKeyID: 'AKIAIMQVLS3FLM3WQDXA',
+				AWSSecretKey: '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'
+			}
 		},
 		templatesPath: '../../views/emails/templates',
 		attachmentsPath: 'attachments'
 	}
 });
 
-// Flash().with('email', 'SES').send(
-// 	//settings
-// 	{
-// 		from: 'noreply@i4-community.com',
-// 		to: 'mickael@sagacify.com',
-// 		lang: 'en',
-// 		type: 'invitation'
-// 	},
-// 	// data
-// 	{
-// 		user: {
-// 			name: 'Mickael van der Beek'
-// 		},
-// 		link: 'http://www.google.be'
-// 	},
-// 	//options
-// 	{
-// 		ensureSuccess: true
-// 	},
-// 	// callback
-// 	function () {
-// 		console.log('Email sent !');
-// 		console.log(arguments);
-// 	}
-// );
-
-Flash().with('email').receive(function () {
-	console.log('Received email !');
-	console.log(arguments);
-});
+Flash().with('email', 'SES').send(
+	//settings
+	{
+		from: 'noreply@i4-community.com',
+		to: 'mickael@sagacify.com',
+		lang: 'en',
+		type: 'invitation'
+	},
+	// data
+	{
+		user: {
+			name: 'Mickael van der Beek'
+		},
+		link: 'http://www.google.be'
+	},
+	//options
+	{
+		ensureSuccess: true
+	},
+	// callback
+	function () {
+		console.log('Email sent !');
+		console.log(arguments);
+	}
+);
