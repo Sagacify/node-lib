aed00b46b2d2fa64df451b70f9c122c42036a7f1
Mickael van der Beek <mickael@sagacify.com>
2014-02-07 14:52:40 +0100

    more examples

---
+++ communication/examples/send_with_Direct.js
@@ -0,0 +1,37 @@
+var Flash = require('../Flash');
+
+var Flash = new Flash({
+	email: {
+		sender: {
+			direct: {}
+		},
+		templatesPath: '../../views/emails/templates',
+		attachmentsPath: 'attachments'
+	}
+});
+
+Flash().with('email', 'direct').send(
+	//settings
+	{
+		from: 'mickael-167018@@vps22813.ovh.net',
+		to: 'mickael@sagacify.com',
+		lang: 'en',
+		type: 'invitation'
+	},
+	// data
+	{
+		user: {
+			name: 'Mickael van der Beek'
+		},
+		link: 'http://www.google.be'
+	},
+	//options
+	{
+		ensureSuccess: true
+	},
+	// callback
+	function () {
+		console.log('Email sent !');
+		console.log(arguments);
+	}
+);
