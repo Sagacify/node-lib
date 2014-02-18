ea3bcc279fe5a037b103b40a3c4d84ea3e316a6e
François Beuvens <francois.beuvens@gmail.com>
2014-02-18 18:54:10 +0100

    ok

--- authentication/auth-logic/remove_ExpiredTokens.js
+++ authentication/auth-logic/remove_ExpiredTokens.js
@@ -1,11 +1,14 @@
 module.exports = function (mixin, callback) {
 	var i = mixin.user.tokens.length;
 	var fresh_tokens = [];
+	console.log('REMOVE EXPIRED TOKENS')
+	console.log(mixin.user.tokens)
 	while(i--) {
 		if(mixin.user.tokens[i].expiration > Date.now()) {
-			fresh_tokens.push(mixin.user.tokens[i]);
+			fresh_tokens.shift(mixin.user.tokens[i]);
 		}
 	}
+	console.log(fresh_tokens);
 	mixin.user.tokens = fresh_tokens;
 	callback(null, mixin);
 };
