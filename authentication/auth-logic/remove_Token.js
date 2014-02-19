69352a162bb43eddd2278704c6064428d347ca83
Mickael van der Beek <mickael@sagacify.com>
2014-02-19 18:30:20 +0100

    error

--- authentication/auth-logic/remove_Token.js
+++ authentication/auth-logic/remove_Token.js
@@ -1,6 +1,6 @@
 module.exports = function (mixin, callback) {
 	console.log('REMOVE TOKEN')
-	console.log(new Error().stack)
+	console.log(new SGError().stack)
 	mixin.user.tokens.splice(mixin.token_match, 1);
 	callback(null, mixin);
 };
