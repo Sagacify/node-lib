14b3bc36bc7c3c8bbe94f85784caba61adb05305
Simon Debaecke <simon@sagacify.com>
2014-07-16 14:52:52 +0200

    add async

--- prototypes/lib/async.js
+++ prototypes/lib/async.js
@@ -2,27 +2,44 @@ var async = require('async');
 
 //Callback = function(err, resultsArray)
 // async.prototype.eachOrder = function(array, iterator, callback) {
-	// TODO
-	// var resultsArray = [];
-	// async.each(array.indexes(), function(index, callback){
-	// 	iterator(array[index], callback);
-	// }, function(err){
-	// 	if (err) {
-	// 		callback(err);
-	// 		return;
-	// 	};
-	// });
+// TODO
+// var resultsArray = [];
+// async.each(array.indexes(), function(index, callback){
+// 	iterator(array[index], callback);
+// }, function(err){
+// 	if (err) {
+// 		callback(err);
+// 		return;
+// 	};
+// });
 // };
 
 async.choose = function (value, functions, callback) {
-	if (!value) {
+	// Missing value
+	if (value === null) {
 		throw new Error("You have to specify the preferred value.");
 	}
-	if (!(value in functions)) {
-		return callback();
+
+	// Check if value is an option in functions dictionary
+	var check = function (value) {
+		if (!(value in functions)) {
+			return false;
+		}
+	};
+
+	// If value is not found and is false (except null), then check for "false" value.
+	if (!check(value)) {
+		if (value) {
+			return callback();
+		}
+
+		value = false;
+		if (!check(value)) {
+			return callback();
+		}
 	}
 
-	functions[value](function() {
+	functions[value](function () {
 		if (callback) {
 			callback.apply(this, arguments);
 		}
